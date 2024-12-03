86f68fe0791543eb1638b411a69d48b0628a25dd
Ntombekhaya Mkaba <ntombekhayamkaba5@gmail.com>
2024-12-03 12:01:00 +0200

    new

--- script.js
+++ script.js
@@ -245,5 +245,28 @@ document.getElementById('contact-form').addEventListener('submit', function(even
       }, (error) => {
         alert('Error sending message: ' + error.text);
       });
+
+//       // Smooth scroll for navigation links
+//   document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute('href')).scrollIntoView({
+//       behavior: 'smooth'
+//     });
+//   });
+// });
+let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+      let slides = document.getElementsByClassName("mySlides");
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+      slides[slideIndex-1].style.display = "block";  
+      setTimeout(showSlides, 3000); // Change image every 3 seconds
+  }
   });
 
